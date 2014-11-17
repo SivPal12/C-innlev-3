568fd50b2b8b240de0b9f5dd02aee10fa043ab67
Pal M Sivertsen <palmarius@hotmail.com>
2014-11-17 22:03:19 +0100

    pseudocode

--- program.c
+++ program.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const int with = 10, height = 20;
+int canvas[with][height];
+
 int main (int argc, char *argv[]) {
-  printf("Hello world");
+  // Setup screen
+  while (true) {
+    // ReadUserInput
+    // UpdateGameState
+    // RenderGame
+  }
 }
